b5f72a2010f5b100199c0a4769a8051547e522d8
jlvivero <josluivivgar@gmail.com>
2016-03-16 15:24:33 -0700

    added basic functionality to the drawing app, missing the retrofit, save options

---
+++ PicassoApp/app/src/main/java/picasso/picassoapp/DrawingView.java
@@ -0,0 +1,113 @@
+package picasso.picassoapp;
+
+import android.graphics.Color;
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+//second part of the imports
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+
+/**
+ * Created by jl on 3/9/16.
+ */
+public class DrawingView extends View
+{
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+
+    public DrawingView(Context context, AttributeSet attribute)
+    {
+        super(context,attribute);
+        setupDrawing();
+    }
+
+    private void setupDrawing()
+    {
+        //get drawing area setup for interaction
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+
+        //initial path properties research more on this, this might be important later
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        //instantiating canvas paint object
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh)
+    {
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        //draw view
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        //detect user touch
+        float touchX = event.getX();
+        float touchY = event.getY();
+        //okay this is the tricky part, FROM THIS I HAVE TO GET ARRAY OF COORDINATES
+        // YEAH I KNOW FUCK MY LIFE, but i have to do this properly, so probably retrofit will go
+        // here RIP ME LOL
+        //okay i found something interesting that ill try later
+        //basically i have my array right?
+        // everytime i get an event i get the coordinates so before i do drawPath.lineTo
+        // i save those coordinates
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    public void setColor(String newColor)
+    {
+        //sets color
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+}
+
--- PicassoApp/app/src/main/java/picasso/picassoapp/MainActivity.java
+++ PicassoApp/app/src/main/java/picasso/picassoapp/MainActivity.java
@@ -1,13 +1,73 @@
 package picasso.picassoapp;
 
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.os.Build.VERSION;
 
-public class MainActivity extends AppCompatActivity {
+//app imports
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity
+{
+    private DrawingView drawView;
+    private ImageButton currPaint;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //this instantiates our drawing view class on our actual gui view named drawing in this case
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+        //retrieves the first paint color thingy
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+
+        //getting the first button and storing it in a button instance
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+
+        //this changes the icon to selected(to the first icon)
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+        {
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+        }
+        else
+        {
+            currPaint.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.paint_pressed));
+
+        }
+    }
+
+    public void paintClicked(View view)
+    {
+        //use chosen color
+        if(view != currPaint)
+        {
+            //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+
+            //this changes the icon selected to the one the user selects
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+            {
+                imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+                currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            }
+            else
+            {
+                imgView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.paint_pressed));
+                currPaint.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.paint));
+            }
+            currPaint=(ImageButton)view;
+        }
+
+
+
     }
 }
